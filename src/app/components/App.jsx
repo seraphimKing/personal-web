59b4afe90c7e508fe4a72fc7b4163a52feda61c4
金婷 <jinting@zhejianglab.com>
2025-02-06 19:38:03 +0800

    Updates

--- src/app/components/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
- 
-const App = () => {
-  return <div>Hello, Welcome</div>;
-};
- 
-export default App;
--- src/app/more/page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function More() {
-  return (
-    <div>
-      More
-      <div />
-    </div>
-  )
-}
